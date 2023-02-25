7a9503d1aa359d9768d97538311d9bb4456295f3
Rohan <rohan@kraftdesignsmiths.com>
2023-02-25 06:41:12 +0000

    feat(tailwind): added tailwind css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,13 +9,15 @@ export const metadata: Metadata = {
 	description: 'Get notified when someone scans your QR code',
 };
 
+import '~/styles/globals.css'
+
 export default function RootLayout(  { children}: {
 	children: ReactNode;
 } ) {
   return (
     <html>
-			<body className="xxx">
-				<main  className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+			<body>
+				<main  className="">
 					{children}
 				</main>
 			</body>
